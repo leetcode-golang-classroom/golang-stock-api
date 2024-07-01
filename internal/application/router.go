35f972ca1df352ce3d34ee4c8f961719b5fd7997
gson Liang <yuanyu90221@gmail.com>
2024-07-02 02:47:35 +0800

    feat(modify folder structure): modify folder structure for stock
    modify folder structure

--- internal/application/router.go
+++ internal/application/router.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/leetcode-golang-classroom/golang-stock-api/internal/stock"
+	"github.com/leetcode-golang-classroom/golang-stock-api/internal/services/stock"
 )
 
 func NewRouter() *mux.Router {
